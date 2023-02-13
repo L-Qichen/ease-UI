56c5f5288125e69ab1ce81d08d5a01fcc8db37ce
Toby <5722599@gmail.com>
2023-02-13 16:42:44 -0500

    Doc.vue layout css

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,26 +1,28 @@
 <template>
-  <topnav />
-  <div class="content">
-    <aside v-if="menuVisible">
-      <h2>Components</h2>
-      <ol>
-        <li>
-          <router-link to="/doc/switch">Switch</router-link>
-        </li>
-        <li>
-          <router-link to="/doc/button">Button</router-link>
-        </li>
-        <li>
-          <router-link to="/doc/dialogue">Dialogue</router-link>
-        </li>
-        <li>
-          <router-link to="/doc/tabs">Tabs</router-link>
-        </li>
-      </ol>
-    </aside>
-    <main>
-      <router-view />
-    </main>
+  <div class="layout">
+    <topnav class="nav" />
+    <div class="content">
+      <aside v-if="menuVisible">
+        <h2>Components</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/switch">Switch</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/button">Button</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/dialogue">Dialogue</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/tabs">Tabs</router-link>
+          </li>
+        </ol>
+      </aside>
+      <main>
+        <router-view />
+      </main>
+    </div>
   </div>
 </template>
 
@@ -38,11 +40,44 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  > .nav {
+    flex-shrink: 0;
+  }
+  > .content {
+    flex-grow: 1;
+    padding-top: 60px;
+    padding-left: 12.5rem;
+    @media (max-width: 500px) {
+      padding-left: 0;
+    }
+  }
+}
+.content {
+  display: flex;
+  > aside {
+    flex-shrink: 0;
+  }
+  > main {
+    border: 1px solid blue;
+    flex-grow: 1;
+    padding: 16px;
+    background: lightgreen;
+  }
+}
 aside {
   border: 1px solid red;
   background: lightblue;
   width: 12rem;
   padding: 1rem;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding-top: 70px;
+  height: 100%;
   > h2 {
     margin-bottom: 4px;
   }
@@ -51,11 +86,8 @@ aside {
       padding: 4px 0;
     }
   }
-  @media (max-width: 500px) {
-    position: fixed;
-    top: 0;
-    left: 0;
-    padding-top: 70px;
+  main {
+    overflow: auto;
   }
 }
 </style>
