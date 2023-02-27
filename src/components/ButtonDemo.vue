21755ed4c5b83272c1805c9a23702dfe6f944cdb
Toby <5722599@gmail.com>
2023-02-27 15:42:16 -0500

    added theme dynamic class for button component

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <Button> Button </Button>
+    <Button theme="button"> Button </Button>
+    <Button theme="link"> Button </Button>
+    <Button theme="text"> Button </Button>
   </div>
 </template>
 
@@ -9,5 +12,17 @@ import Button from "../lib/Button.vue";
 
 export default {
   components: { Button },
+  setup() {
+    const onClick = () => {
+      console.log("hi");
+    };
+    return { onClick };
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+div {
+  border: 1px solid red;
+}
+</style>
