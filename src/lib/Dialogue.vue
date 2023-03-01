105dcb6c28b2fbd778a7bc2273a743c142389a5e
Toby <5722599@gmail.com>
2023-02-28 20:14:44 -0500

    initialize Dialogue component file and html

---
+++ src/lib/Dialogue.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="ease-dialogue-overlay"></div>
+  <div class="ease-dialogue-wrapper">
+    <header>Title</header>
+    <main>
+      <p>First sentence</p>
+      <p>Second sentence</p>
+    </main>
+    <footer>
+      <Button>Confirm</Button>
+      <Button>Cancel</Button>
+    </footer>
+  </div>
+</template>
+
+<script lang="ts">
+import Button from "../lib/Button.vue";
+
+export default {
+  components: {
+    Button,
+  },
+};
+</script>
