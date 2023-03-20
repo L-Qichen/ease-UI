022c230d246410e57e2c766db9c21bc1743ce52e
Toby <5722599@gmail.com>
2023-03-20 17:19:45 -0400

    Added content to installation page and get-started page

--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,3 +1,10 @@
 <template>
-  <div>Install</div>
+  <article>
+    <h1>Installation</h1>
+    <p>Open Terminal and run the following command:</p>
+    <pre><code>$ yarn add ease-ui</code></pre>
+    <p>or</p>
+    <pre><code>$ npm install ease-ui</code></pre>
+    <p>Next:<a href="#/doc/get-started">Get Started</a></p>
+  </article>
 </template>
