40e484d54d2dd91ee72af1f638246802f6ced64e
Toby <5722599@gmail.com>
2023-03-21 14:40:59 -0400

    Now support writing and import markdown file

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,27 +1,14 @@
 <template>
-  <article class="markdown-body">
-    <h1>Introduction</h1>
-    <p>
-      I wrote EASE UI for study purposes. It's a UI library based on Vue 3 and
-      Typescript.
-    </p>
-    <p>
-      The process of writing this UI library was a great learning experience for
-      me. All the design and implementation decisions are made based on my
-      experience as a web developer. Everything was written by myself, including
-      this documentation website, and I tried to avoid using other libraries and
-      dependencies as much as I could. Therefore, I don't really recommend using
-      this UI library in production.
-    </p>
-    <p>
-      However, if you wanted to checkout the source code, then you're most
-      definitely in the right place. All the source code is stored in a public
-      repository, here's the link: https://github.com/L-Qichen/ease-UI. The
-      history commit information shows very clearly what I have done, and you
-      can view them one by one in commit order. You can also view the source
-      code and examples for each component directly and run the project
-      following the steps described in README.md.
-    </p>
-    Next:<a href="#/doc/install">Installation</a>
-  </article>
+  <article class="markdown-body" v-html="md"></article>
 </template>
+
+<script>
+import md from "../markdown/intro.md";
+export default {
+  data() {
+    return {
+      md,
+    };
+  },
+};
+</script>
