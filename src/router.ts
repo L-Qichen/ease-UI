d434bd38f5823a9f132c460013ee11096155638a
Toby <5722599@gmail.com>
2023-03-23 22:15:43 -0400

    Further optimized the code, use h() dynamic generated vnode to replace 3 .vue files

--- src/router.ts
+++ src/router.ts
@@ -6,11 +6,11 @@ import ButtonDemo from './components/ButtonDemo.vue'
 import DialogueDemo from './components/DialogueDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
 import WelcomeDocs from './components/WelcomeDocs.vue'
-import Intro from './views/Intro.vue'
-import GetStarted from './views/GetStarted.vue'
-import Install from './views/Install.vue'
+import Markdown from "./components/Markdown.vue"
+import { h } from 'vue'
 
 const history = createWebHashHistory();
+const md = fileName => h(Markdown, { path: `../markdown/${fileName}.md`, key: fileName });
 export const router = createRouter({
   history,
   routes: [
@@ -19,9 +19,9 @@ export const router = createRouter({
       path: '/doc', component: Doc,
       children: [
         { path: '', component: WelcomeDocs },
-        { path: 'intro', component: Intro },
-        { path: 'get-started', component: GetStarted },
-        { path: 'install', component: Install },
+        { path: 'intro', component: md("intro") },
+        { path: 'get-started', component: md("get-started") },
+        { path: 'install', component: md("install") },
         { path: 'switch', component: SwitchDemo },
         { path: 'Button', component: ButtonDemo },
         { path: 'Dialogue', component: DialogueDemo },
