ba90275e589fb94f7109ad574807ee39f2248a56
Toby <5722599@gmail.com>
2023-04-01 14:00:05 -0400

    changed router file, allow rollup(yarn build) parse the path

--- src/router.ts
+++ src/router.ts
@@ -6,10 +6,13 @@ import ButtonDemo from './components/ButtonDemo.vue'
 import DialogueDemo from './components/DialogueDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
 import Markdown from "./components/Markdown.vue"
+import intro from './markdown/intro.md'
+import getStarted from './markdown/get-started.md'
+import install from './markdown/install.md'
 import { h } from 'vue'
 
 const history = createWebHashHistory();
-const md = fileName => h(Markdown, { path: `../markdown/${fileName}.md`, key: fileName });
+const md = string => h(Markdown, { content: string, key: string });
 export const router = createRouter({
   history,
   routes: [
@@ -18,9 +21,9 @@ export const router = createRouter({
       path: '/doc', component: Doc,
       children: [
         { path: '', redirect: '/doc/intro' },
-        { path: 'intro', component: md("intro") },
-        { path: 'get-started', component: md("get-started") },
-        { path: 'install', component: md("install") },
+        { path: 'intro', component: md(intro) },
+        { path: 'get-started', component: md(getStarted) },
+        { path: 'install', component: md(install) },
         { path: 'switch', component: SwitchDemo },
         { path: 'Button', component: ButtonDemo },
         { path: 'Dialogue', component: DialogueDemo },
