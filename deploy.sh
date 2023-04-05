5bd9dc45eae499012b0aad4435d28e50bda7e908
Toby <5722599@gmail.com>
2023-04-05 10:31:44 -0400

    Fixed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ rm -rf dist &&
  git init &&
  git add . &&
  git commit -m "update" &&
- git branch -M master &&
+ git branch -M main &&
  git remote add origin git@github.com:L-Qichen/ease-ui-website.git &&
- git push -f -u origin master &&
+ git push -f -u origin main &&
  cd -
  echo https://l-qichen.github.io/ease-ui-website/index.html 
