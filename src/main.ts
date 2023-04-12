3ceec669227f43b80bb6512e4b3cc95cf39e3ffc
Toby <5722599@gmail.com>
2023-04-12 10:56:38 -0400

    moved svg.js to public

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import './index.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
-import './lib/svg.js'
+import '../public/svg.js'
 import 'github-markdown-css'
 import Markdown from "./components/Markdown.vue"
 import "/fonts.css"
